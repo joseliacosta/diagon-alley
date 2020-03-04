4636290389d2485712d4626e13c58706cc44df65
Josélia L. Costa <joselialcosta@gmail.com>
2020-03-04 20:31:26 -0300

    Adds tests to check if the correct house was rendered

--- src/components/wizard/wizard.test.js
+++ src/components/wizard/wizard.test.js
@@ -2,17 +2,47 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import Wizard from './wizard'
 
-test('renders wizard name', () => {
+describe('Wizard', () => {
+  let wizardMock
+
+  beforeEach(() => {
+    wizardMock = {
+      name: 'Harry',
+      school: 'Hogwarts School of Witchcraft and Wizardry',
+      house: 'Gryffindor',
+      bloodStatus: 'half-blood',
+      dumbledoresArmy: true,
+    }
+  })
+
+  test('renders wizard name', () => {
+    const { getByText } = render(<Wizard {...wizardMock} />)
+    const wizardName = getByText(/Harry/i)
+
+    expect(wizardName).toBeInTheDocument()
+  })
+
+  test('renders wizard house', () => {
+    const { getByText, getByAltText } = render(<Wizard {...wizardMock} />)
+    const wizardHouse = getByText(/Gryffindor/i)
+    const altImage = getByAltText("Gryffindor's logo")
+
+    expect(wizardHouse).toBeInTheDocument()
+    expect(altImage).toBeInTheDocument()
+  })
+})
+
+test('renders harry potter original logo', () => {
   const wizardMock = {
-    name: 'Harry',
-    school: 'Hogwarts School of Witchcraft and Wizardry',
-    house: 'Gryffindor',
-    bloodStatus: 'half-blood',
+    name: 'Joselia',
+    school: 'Faculdade de Tecnologia Termomecanica',
+    house: 'ADS',
+    bloodStatus: 'unknow',
     dumbledoresArmy: true,
   }
 
-  const { getByText } = render(<Wizard {...wizardMock} />)
-  const wizardName = getByText(/Harry/i)
+  const { getByAltText } = render(<Wizard {...wizardMock} />)
+  const altImage = getByAltText('Harry Potter Logo')
 
-  expect(wizardName).toBeInTheDocument()
+  expect(altImage).toBeInTheDocument()
 })
