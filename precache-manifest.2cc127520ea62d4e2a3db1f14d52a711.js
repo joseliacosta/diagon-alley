08a68b044048303594c112ebcdf903fc454b2458
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-29 02:56:41 +0000

    Deploy joselialcosta/diagon-alley to github.com/joselialcosta/diagon-alley.git:gh-pages

---
+++ precache-manifest.2cc127520ea62d4e2a3db1f14d52a711.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "970d2b064c447c3ba6b0db03536d98e9",
+    "url": "/diagon-alley/index.html"
+  },
+  {
+    "revision": "08221490a2cb68c84c1e",
+    "url": "/diagon-alley/static/css/main.e71a7498.chunk.css"
+  },
+  {
+    "revision": "284367a84da77bd0d6f3",
+    "url": "/diagon-alley/static/js/2.f1f74cd1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/diagon-alley/static/js/2.f1f74cd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08221490a2cb68c84c1e",
+    "url": "/diagon-alley/static/js/main.4087ab20.chunk.js"
+  },
+  {
+    "revision": "7a1de285656b541577fe",
+    "url": "/diagon-alley/static/js/runtime-main.f24f4242.js"
+  }
+]);
